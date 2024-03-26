a21a4bb50ca7731a5119c7dcb1353d4537626a72
Knowledge Syndicate <k.syndicate.school@gmail.com>
2024-03-26 20:54:49 +0100

    l10-presetup

---
+++ src/ecs-survivors/Assets/Code/Infrastructure/Serialization/JsonSerialization.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Utilities;
+
+namespace Code.Infrastructure.Serialization
+{
+  public static class JsonSerialization
+  {
+    public static string ToJson(this object self) =>
+      JsonConvert.SerializeObject(self, new JsonSerializerSettings
+      {
+        TypeNameHandling = TypeNameHandling.Auto,
+        Formatting = Formatting.Indented,
+        TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+      });
+
+    public static T FromJson<T>(this string json) =>
+      JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings
+      {
+        TypeNameHandling = TypeNameHandling.Auto,
+        TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple
+      });
+
+    static JsonSerialization() => 
+      AotHelper.Ensure(() => new ReferenceConverter(typeof(Dummy)));
+    
+    public class Dummy{}
+  }}
